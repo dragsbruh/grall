9a428da424040873a5c37151dcde78e7558043e1
dragsbruh <dragsbruh@protonmail.com>
2025-09-02 11:11:48 +0530

    chore: add an alternate revCmp that isnt as fast but has potential, also its incorrect but this is just a backup so its fine

--- lib/util.zig
+++ lib/util.zig
@@ -8,5 +8,18 @@ pub fn revCmp(a: []const u8, b: []const u8) std.math.Order {
         if (a[i] < b[i]) return .lt else if (a[i] > b[i]) return .gt;
     }
 
+    // const size = (a.len + 7) / 8;
+    // var i = size;
+    // while (i > 0) {
+    //     i -= 1;
+
+    //     const end = @min((i + 1) * 8, a.len);
+
+    //     const aa = std.mem.readVarInt(u64, a[i * 8 .. end], .big);
+    //     const bb = std.mem.readVarInt(u64, b[i * 8 .. end], .big);
+    //     if (aa > bb) return .gt;
+    //     if (bb > aa) return .lt;
+    // }
+
     return .eq;
 }
