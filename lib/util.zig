223650d98d294c28da056638f0d90981cf6952c8
dragsbruh <dragsbruh@protonmail.com>
2025-09-02 12:23:38 +0530

    perf: slightly faster revCmp probably

--- lib/util.zig
+++ lib/util.zig
@@ -2,24 +2,17 @@ const std = @import("std");
 
 /// assumes a.len = b.len
 pub fn revCmp(a: []const u8, b: []const u8) std.math.Order {
-    var i = a.len;
-    while (i > 0) {
-        i -= 1;
-        if (a[i] < b[i]) return .lt else if (a[i] > b[i]) return .gt;
+    var i: usize = a.len;
+    while (i >= 8) : (i -= 8) {
+        const aa = std.mem.bytesToValue(u64, a[i - 8 .. i]);
+        const bb = std.mem.bytesToValue(u64, b[i - 8 .. i]);
+        if (aa != bb) return if (aa < bb) .lt else .gt;
     }
 
-    // const size = (a.len + 7) / 8;
-    // var i = size;
-    // while (i > 0) {
-    //     i -= 1;
-
-    //     const end = @min((i + 1) * 8, a.len);
-
-    //     const aa = std.mem.readVarInt(u64, a[i * 8 .. end], .big);
-    //     const bb = std.mem.readVarInt(u64, b[i * 8 .. end], .big);
-    //     if (aa > bb) return .gt;
-    //     if (bb > aa) return .lt;
-    // }
+    while (i > 0) {
+        i -= 1;
+        if (a[i] != b[i]) return if (a[i] < b[i]) .lt else .gt;
+    }
 
     return .eq;
 }
